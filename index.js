03a348a424213a849c64dbaf5d7fba977edbe173
Sujait <mdsujait2000@gmail.com>
2023-11-12 18:14:29 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -9,6 +10,23 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+  //middleware jwt
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+//middleware jwt end
 
 // mongodb start
 
@@ -34,44 +52,78 @@ async function run() {
     const menuCollection = client.db("myrestaurant").collection("menu");
     const reviewCollection = client.db("myrestaurant").collection("reviews");
     const cartCollection = client.db("myrestaurant").collection("carts");
-    
+
+
+    //client side theka data pata bo ja server site a playLoad hisaba data server site a set hoba.
+    //JWT Api Create
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+//Admine Verify middleware....
+
+const verifyAdmin = async(req,res,next) =>{
+  const email = req.decoded.email;
+  const query = {email:email}
+  const user = await usersCollection.findOne(query);
+  if(user?.role !== 'admin'){
+    return res.status(403).send({error:true, message: 'forbidden message'})
+  }
+  next();
+}
+
+
 
 
     //users api
-    app.get('/users', async(req,res) =>{
+    app.get('/users', verifyJWT,verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
 
     // users information related api
-     app.post('/users',async(req,res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
       // Checked userCollection Email Present or Not 
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await usersCollection.findOne(query)
-      if(existingUser){
-        return res.send({message:'user already exists'})
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
       }
       const result = await usersCollection.insertOne(user)
       res.send(result);
-     })
+    })
 
-     //Admin Create Api 
-app.patch('/users/admin/:id',async(req,res)=>{
-  const id =req.params.id;
-  const filter = {_id: new ObjectId(id)};
-  const updateDoc = {
-    $set:{
-      role:'admin'
-    },
-  };
-  const result =await usersCollection.updateOne(filter,updateDoc);
-  res.send(result);
-})
+    //Admin Create Api 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+//admin chacked Api 
+app.get('/users/admin/:email', verifyJWT ,async (req,res)=>{
+  const email = req.params.email;
+
+  if(req.decoded.email !== email){
+    res.send({admin: false})
+  }
+
+  const query = {email:email}
+  const user = await usersCollection.findOne(query);
+  const result = {admin: user?.role === 'admin'}
+  res.send(result)
+}) 
 
 
-    
-    
     //data read {menu}
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -84,11 +136,18 @@ app.patch('/users/admin/:id',async(req,res)=>{
     })
 
     //cart collection API {data find and data taken}
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
+//jwt
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'porviden access' })
+      }
+//
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result)
@@ -97,7 +156,7 @@ app.patch('/users/admin/:id',async(req,res)=>{
     //cart collection
     app.post('/carts', async (req, res) => {
       const item = req.body;
-  
+
       const result = await cartCollection.insertOne(item)
       res.send(result)
     })
